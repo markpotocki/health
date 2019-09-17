ef5f15989d8d885129668ea63d0c9ecdf5c37627
markpotocki <32443509+markpotocki@users.noreply.github.com>
2019-09-17 13:43:27 -0500

    Add client info handler (#15)
    * remove this
    
    * remove binaries
    
    * unlock the mutex when finished
    
    * add dynamic admin port registering for clients
    
    * add comptability for not selecting port
    
    * add handler to display data
    
    *allows client data to be accessed
    *sets update to 1s from 10s - allows for quicker client side updates.

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -14,6 +15,10 @@ func (srv *Server) registerHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&clientInfo)
 
+	if clientInfo.CPort == 0 {
+		clientInfo.CPort = 9999 // for backwards compatability
+	}
+
 	if err != nil {
 		log.Printf("server-register: bad type recieved %v", err)
 		http.Error(w, "not expected json", http.StatusBadRequest)
@@ -22,7 +27,7 @@ func (srv *Server) registerHandler(w http.ResponseWriter, r *http.Request) {
 	clientAddr := r.RemoteAddr
 	splited := strings.Split(clientAddr, ":")
 	if baseurl := splited[0]; baseurl != "" {
-		clientInfo.CURL = "http://" + baseurl + ":9999/metrics/health"
+		clientInfo.CURL = fmt.Sprintf("http://%s:%d/metrics/health", baseurl, clientInfo.CPort)
 	}
 
 	srv.clientStore.Save(clientInfo)
@@ -33,4 +38,37 @@ func (srv *Server) registerHandler(w http.ResponseWriter, r *http.Request) {
 
 func (srv *Server) readyHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK) // can add checks for whatever here
+}
+
+func (srv *Server) clientInfoHandler(w http.ResponseWriter, r *http.Request) {
+	// start with http://localhost:0/info/param
+	httpTrim := strings.TrimPrefix(r.RequestURI, "http://")
+	httpTrim = strings.TrimPrefix(r.RequestURI, "https://")
+	httpTrim = strings.TrimPrefix(httpTrim, "/")
+	log.Println("FIND ME::  " + httpTrim)
+	// localhost:0/aidi/info/param/
+	split := strings.Split(httpTrim, "/")
+	if len(split) > 4 {
+		log.Println("server: invalid path in info handler")
+		http.Error(w, "Not Found", http.StatusNotFound)
+	} else if len(split) == 4 {
+		info := srv.statusStore.Find(split[3])
+		if info.ClientName == "" {
+			http.Error(w, "could not find the requested client", http.StatusNotFound)
+		} else {
+			err := json.NewEncoder(w).Encode(&info)
+			if err != nil {
+				log.Printf("server: encountered error decoding json: %v", err)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+			}
+		}
+	} else {
+		info := srv.statusStore.FindAll()
+		err := json.NewEncoder(w).Encode(&info)
+		if err != nil {
+			log.Printf("server: encountered error decoding json: %v", err)
+			http.Error(w, "internal error", http.StatusInternalServerError)
+		}
+	}
+
 }
